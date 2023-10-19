bbb4df0ae95d5f603251e2ad1edbb080986f66ab
Enantoka <enantoka@gmail.com>
2023-10-19 16:31:13 +1100

    Test

--- components/nav/top_nav.js
+++ components/nav/top_nav.js
@@ -2,10 +2,10 @@ document.querySelector('#top-nav').innerHTML = `
 <nav class="navigation-bar">
     <ul>
         <li class="navigation-item">
-            <a class="nav-link" href="/">Home</a>
+            <a class="nav-link" href="https://enantoka.github.io/Vanilla-JS-Portfolio/">Home</a>
         </li>
         <li class="navigation-item">
-            <a class="nav-link" href="pages/dot_maker.html">Dot Maker</a>
+            <a class="nav-link" href="https://enantoka.github.io/Vanilla-JS-Portfolio/pages/dot_maker.html">Dot Maker</a>
         </li>
         <li class="navigation-item">
             <a class="nav-link" href="https://enantoka.github.io/Practice-1/">Abstract</a>
